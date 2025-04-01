bd93281a0737ebb99bea7f62093f912639109216
David Lundholm <david@kumpan.se>
2025-04-01 20:08:03 +0200

    Add vitest.config and memfs

--- packages/cli/src/ui.ts
+++ packages/cli/src/ui.ts
@@ -4,3 +4,11 @@ export const printAvailableItems = (scope: Scope) => {
   const itemNames = scope.items.map((item) => item.name);
   console.log(`Available ${scope.name}s`, itemNames);
 };
+
+export const printWarning = (...msgs: string[]) => {
+  console.warn(`Warning: ${msgs.join(", ")}`);
+};
+
+export const printError = (...msgs: string[]) => {
+  console.error(`Error: ${msgs.join(", ")}`);
+};
