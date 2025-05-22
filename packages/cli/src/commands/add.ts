01bdea988486d17a75c74a77c3a6713ffae83f50
David Lundholm <david@kumpan.se>
2025-05-22 14:40:48 +0200

    Add installDependencies to add-command

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -1,6 +1,7 @@
 import { Argument, Command } from "@commander-js/extra-typings";
 import { singular } from "pluralize";
 import { copyItem } from "../lib/copyItem";
+import { installDependencies } from "../lib/installDependencies";
 import type { Scope } from "../lib/types";
 import * as UI from "../lib/ui";
 
@@ -15,12 +16,13 @@ export const buildAddCommand = (scopes: Scope[]) => {
         .argument("[path]")
         .option("-l, --list", `List available ${scope}`)
         .usage("<scope> [item] [path]")
-        .action((itemName, path, opts) => {
+        .action(async (itemName, path, opts) => {
           const item = itemName ? scope.items.find((item) => item.name === itemName) : null;
           if (opts.list) {
             UI.printAvailableItems(scope);
           } else if (item && path) {
             copyItem(item, path);
+            await installDependencies(item, path);
           } else if (!itemName) {
             UI.printAvailableItems(scope);
           } else {
